21659e9be7c2ab1ddd4fe16809f4ffc446df96ce
pjj1214@163.com <pjj1214@163.com>
2019-12-02 22:15:24 +0800

    commit by vscode

--- src/componnets/Home.js
+++ src/componnets/Home.js
@@ -5,14 +5,51 @@ class Home extends Component {
         super(props);
         this.key = 1;
         this.state={
-            name: 'ryan'
+            name: 'ryan',
+            msg: '我是msg'
         }
+        this.getName=this.getName.bind(this);
     }
-
+    run() {
+        alert('run');
+    }
+    getMsg() {
+        alert(this.state.msg);
+    }
+    getName() {
+        alert(this.state.name);
+    }
+    getMessage= () => {
+        alert(this.state.msg)
+    }
+    setMsg(){
+        this.setState({
+            msg:'我是改变了的msg'
+        })
+    }
+    setMessage= ()=>{
+        this.setState({
+            msg:'箭头函数绑定this改变的msg'
+        })
+    }
+    setName =(name) =>{
+        this.setState({
+            name
+        })
+    }
     render() {
-        return <div>我是Home组件
-            {this.state.name}
-        </div>
+        return (<div>我是Home组件
+            {this.state.name}<br/>
+            {this.state.msg}
+            <hr/>
+            <button onClick={this.run}>run</button><br/>
+            <button onClick={this.getMsg.bind(this)}>第一种方式绑定this</button><br/>
+            <button onClick={this.getName}>第二种绑定this</button><br/>
+            <button onClick={this.getMessage}>第三种绑定this</button><br/>
+            <button onClick={this.setMsg.bind(this)}>setMsg</button> <br/>
+            <button onClick={this.setMessage}>setMessage</button><br/>
+            <button onClick={this.setName.bind(this,'jade')}>setName</button>
+        </div>)
         
     }
 }
