a9ef179bb8a1f4f1c1000c8ce55416edc35feed4
pjj1214@163.com <pjj1214@163.com>
2019-12-02 22:15:24 +0800

    commit by vscode

--- src/componnets/News.js
+++ src/componnets/News.js
@@ -1,14 +1,38 @@
 import React from 'react';
-
+import '../asserts/css/News.css';
+import logo from '../asserts/images/logo.svg'
 class News extends React.Component {
     constructor() {
         super();
         this.state={
-            news:'震惊'
+            news:'震惊',
+            color:'red',
+            list:[1111,22222,3333],
+            list2:[{title:1},{title:2},{title:3}]
         }
     }
     render() {
-    return <div>{this.state.news}</div>
+        let listResult = this.state.list.map((value, index) =>{
+            return <li key={index}>{value}</li>
+        });
+    return (<div>{this.state.news}
+        <div className='news'>news</div>
+        <div style={{color:'blue'}}>test style</div>
+        <div style={{backgroundColor:this.state.color}}>bgc red</div>
+        <img src={logo} alt='' width='100'/>
+        <label htmlFor='name'>姓名</label>
+        <input type='text' id='name'/>
+        <hr/>
+        {listResult}
+        <hr/>
+        <ul>
+            {
+                this.state.list2.map((item, index) => {
+                return <li key={index}>{item.title}</li>
+                })
+            }
+        </ul>
+    </div>)
     }
 }
 export default News;
